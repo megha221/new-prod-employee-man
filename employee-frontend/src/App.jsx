a9d00d720a6236fdaab6e5151230eb1032547e8c
meghana <meghana@meghanas-MacBook-Air.local>
2026-06-22 22:40:42 +0530

    cors-frontend

--- employee-frontend/src/App.jsx
+++ employee-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5001/employees";
+const API_URL = "http://3.95.66.212:5001/employees";
 
 function App() {
   const [employees, setEmployees] = useState([]);
